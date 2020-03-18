48d1063214cc01b3dcbc860e2d1c84e4b64360bb
Sivanandan Anjana <anjetnet@gmail.com>
2020-03-19 00:40:21 +0530

    ballerina 1.1.4

--- Formula/ballerina.rb
+++ Formula/ballerina.rb
@@ -1,9 +1,9 @@
 class Ballerina < Formula
   desc "Programming Language for Network Distributed Applications"
   homepage "https://ballerina.io"
-  url "https://github.com/s-anjana/ballerina-lang/archive/v1.1.4.tar.gz"
+  url "https://product-dist.ballerina.io/downloads/1.1.4/ballerina-1.1.4.zip"
 
-  sha256 "801161f339b54fd78df0b223bd5e47fa4e84499dd8d61346a70fcba10c82bb5c"
+  sha256 "63986437a86c5ed054923cc816fc3e0425b41be68ea2d5e701acf6c360b7d03c"
 
 
   bottle :unneeded
