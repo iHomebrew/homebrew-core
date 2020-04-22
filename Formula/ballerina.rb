3c423d4d070d960dfa9621d191550bfef54119b2
Sivanandan Anjana <anjetnet@gmail.com>
2020-04-22 12:48:45 +0530

    ballerina 1.2.1

--- Formula/ballerina.rb
+++ Formula/ballerina.rb
@@ -1,1 +1,34 @@
-Test
+class Ballerina < Formula
+  desc "Programming Language for Network Distributed Applications"
+  homepage "https://ballerina.io"
+  url "https://product-dist.ballerina.io/downloads/1.2.1/ballerina-1.2.1.zip"
+
+  sha256 "24758bb6ce19357bc734cbec5b8d42f710d93235762c5ce5840ee7ef37b8f8c9"
+
+
+  bottle :unneeded
+
+  depends_on :java => "1.8"
+
+  def install
+    # Remove Windows files
+    rm Dir["bin/*.bat"]
+
+    chmod 0755, "bin/ballerina"
+
+    bin.install "bin/ballerina"
+    libexec.install Dir["*"]
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
+  end
+
+  test do
+    (testpath/"helloWorld.bal").write <<~EOS
+      import ballerina/io;
+      public function main() {
+        io:println("Hello, World!");
+      }
+    EOS
+    output = shell_output("#{bin}/ballerina run helloWorld.bal")
+    assert_equal "Hello, World!", output.chomp
+  end
+end
